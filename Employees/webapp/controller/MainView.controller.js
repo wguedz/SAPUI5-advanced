2c4d4d4d052dc05290d702eaa20b898baf5915f6
Wilmer Guedez <wilmergdz.01@gmail.com>
2022-01-04 07:03:50 +0000

    Lab 14 Elementos y Controladores UI5

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,3 +1,4 @@
+// @ts-ignore
 sap.ui.define([
 		"sap/ui/core/mvc/Controller"
 	],
@@ -10,6 +11,26 @@ sap.ui.define([
 		return Controller.extend("logaligroup.Employees.controller.MainView", {
 			onInit: function () {
 
-			}
+			},
+
+            onLiveChange: function () { 
+                var inputEmployee = this.byId("inputEmployee");
+                // @ts-ignore
+                var valueEmployee = inputEmployee.getValue();
+
+                if (valueEmployee.length===6) {
+                    // @ts-ignore
+                    //inputEmployee.setDescription("OK");
+                    this.byId("labelCountry").setVisible(true);
+                    this.byId("slCountry").setVisible(true);
+                }else {
+                    // @ts-ignore
+                    //inputEmployee.setDescription("Not OK");
+                    this.byId("labelCountry").setVisible(false);
+                    this.byId("slCountry").setVisible(false);                    
+                }
+                
+            }            
+
 		});
 	});
